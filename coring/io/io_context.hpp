684719bd92785a9236a5498359c4d4285ea48510
pan <panjunzhong@outlook.com>
2021-11-27 23:52:49 +0800

    func: define the io_context(proactor) abstraction interface (draft ) *notes: there are some questions need to be resolved in future version:  - how do we handle the type T in task<T> ? considering that the task is    usually served as a complete logic flow of a series of network    services, it may help reduce complexity if we force require the task    has to be task<void>.  - if we allow user (of io_context) to bind multiple task, then we need    a queue, simple implementation have potential copyings would down the performance.  - how do we define run and design a timer.  - timer event would be a not small topic, there are many choices would    be required to made, more consideration should be taken in the next    few weeks (not days, limited time can be use for a hard period of school year).  - how to manage the uio resouce, current impl would use raw poitner,    if we adopt shared_ptr, more design alternatives are introduced.  - sadly I didn' t be well prepared and organized with this project.

--- coring/io/io_context.hpp
+++ coring/io/io_context.hpp
@@ -2,7 +2,26 @@
 #ifndef CORING_IO_CONTEXT_HPP
 #define CORING_IO_CONTEXT_HPP
 
+#include "coring/async/task.hpp"
+#include <vector>
 namespace coring {
-class io_context {};
+class io_context {
+ public:
+  void run() {}
+  template <typename T, bool nothrow>
+  T run(const coring::task<T> &t) noexcept(nothrow) {
+    return {};
+  }
+
+ private:
+  // TODO(pan): as far as I know, std::function
+  // can be used to implement true generic container for
+  // template class.
+  // When make everything inherit from a IClass is painful.
+  // But maybe task could be modified to adapt that.
+  // std::vector<coring::task> mission_queue;
+  // or something like that:
+  // std::vector<std::shared_ptr<coring::task_interface>> mission_queue;
+};
 }  // namespace coring
 #endif  // CORING_IO_CONTEXT_HPP
