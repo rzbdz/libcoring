f0a76002a8ad433d5cfedd4f6195ce3abae323fb
pan <panjunzhong@outlook.com>
2021-11-30 15:43:55 +0800

    func: make logging front-end support fmt packeting (use tuple in closure)  - more performance tests should be made.  - current simple bench shows that in 1000,000 scale, the async    packeting version is 50% faster than direct call format_to    (wsl2, g++11 with O2, 7 argument contains str, int, and float formatting).  - in scale of 1000,0000 , their performance are very close (not knowing    reason) with both 7 arguments or 3.  - if the count of format args is 3 (short) and with smaller scale, say    1000 ~ 10, 000, the packeting version take 30% ~ 50% faster than    format in place. *notes: but it's a trade-of between front-end latency and copying to backend    as well as processing overheads. This requires a busy thread doing polling    in the background, but when the log rate is low, most of    the time it should be sleeping.

--- coring/utils/logging/logging.cpp
+++ coring/utils/logging/logging.cpp
@@ -1,2 +1,16 @@
 
 #include "logging.hpp"
+
+namespace coring {
+thread_local timestamp last_time_;
+thread_local char time_buffer_[40];
+}  // namespace coring
+
+namespace coring {
+const char *logger::log_level_map_[] = {"TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"};
+
+logger::logger(logger::file_name_t file, int line, logger::log_level level) : log_entry_{file, line, timestamp{}, level} {}
+
+logger::log_entry::log_entry(logger::file_name_t file, int line, timestamp ts, logger::log_level lv) : file_(file), line_(line), ts_(std::move(ts)), lv_(lv) {}
+
+}  // namespace coring
--- coring/utils/logging/logging.hpp
+++ coring/utils/logging/logging.hpp
@@ -1,58 +1,194 @@
-
+/// front end of logging module
 #ifndef CORING_LOGGING_HPP
 #define CORING_LOGGING_HPP
 #include "../fmt/format.h"
+#include "timestamp.hpp"
+#include <chrono>
+#include <vector>
+#include <functional>
 
-// front end of logging module
+#define CAST_CSTRING
+
+// exposed for testing reason
+struct TEST;
 namespace coring::detail {
-class file_name_converter {
- public:
-  template <int N>
-  file_name_converter(const char (&arr)[N]) : data_(arr), size_(N - 1) {
-    const char *slash = strrchr(data_, '/');  // builtin function
-    if (slash) {
-      data_ = slash + 1;
-      size_ -= static_cast<int>(data_ - arr);
-    }
-  }
 
-  explicit file_name_converter(const char *filename) : data_(filename) {
-    const char *slash = strrchr(filename, '/');
-    if (slash) {
-      data_ = slash + 1;
-    }
-    size_ = static_cast<int>(strlen(data_));
-  }
+template <typename T>
+struct should_trans : public std::false_type {};
 
-  const char *data_;
-  int size_;
+#ifdef CAST_CSTRING
+template <>
+struct should_trans<const char *> : public std::true_type {};
+
+template <>
+struct should_trans<char *> : public std::true_type {};
+
+template <>
+struct should_trans<const char *&> : public std::true_type {};
+
+template <>
+struct should_trans<char *&> : public std::true_type {};
+#endif
+
+template <typename T>
+constexpr bool should_trans_v = should_trans<T>::value;
+
+template <typename T>
+struct trans_type {
+  typedef T use_type;
 };
-class logger_impl {};
-}  // namespace coring::detail
-namespace coring {
 
+template <>
+struct trans_type<char *> {
+  typedef std::string use_type;
+};
+
+template <>
+struct trans_type<const char *> {
+  typedef std::string use_type;
+};
+
+template <typename T>
+auto trans(T v) {
+  // make sure pass by value instead of reference
+  // it's ok when char* is just a pointer
+  using use_type = typename trans_type<T>::use_type;
+  static_assert(!std::is_same_v<use_type, T>);
+  static_assert(std::is_constructible_v<use_type, T>);
+  return use_type(v);
+}
+
+template <typename T>
+struct should_copy : public std::true_type {};
+
+template <int N>
+struct should_copy<const char (&)[N]> : public std::false_type {};
+
+template <typename T>
+constexpr bool should_copy_v = should_copy<T>::value;
+
+template <typename... Args>
+auto make_args_tuple(Args &&...) requires(sizeof...(Args) == 0) {
+  return std::tie();
+}
+
+template <typename Arg, typename... Args>
+auto make_args_tuple(Arg &&first_arg, Args &&...args) {
+  if constexpr (should_copy_v<Arg>) {
+    if constexpr (should_trans_v<Arg>) {
+      return std::tuple_cat(std::make_tuple(trans(first_arg)), make_args_tuple(args...));
+    } else {
+      return std::tuple_cat(std::make_tuple(first_arg), make_args_tuple(args...));
+    }
+  } else {
+    return std::tuple_cat(std::forward_as_tuple(first_arg), make_args_tuple(args...));
+  }
+}
+
+/// Make a tuple since fmt::format_args_store contains both
+/// data (type erased) and type info.
+/// This approach would only generate tight assembly codes.
+/// The data would passed by pointer to the backend.
+/// \tparam Args
+/// \param fmt use string_view, so compile time syntax
+///            test must be done in the very front end
+///            using fmt::format_string<Args...> &&fmt, Args &&...args
+/// \param args
+/// \return
+template <typename... Args>
+auto make_log_tuple(fmt::string_view fmt, Args &&...args) {
+  return std::tuple_cat(std::make_tuple(fmt), make_args_tuple(args...));
+}
+/// make a task that can be runned in backend
+/// the task is for doing fmt formatting.
+/// \tparam Args
+/// \param fmt use string_view, so compile time syntax
+/////          test must be done in the very front end
+/////          using fmt::format_string<Args...> &&fmt, Args &&...args
+/// \param args
+/// \return
+template <typename... Args>
+auto make_log_task(fmt::string_view fmt, Args &&...args) {
+  return [t = make_log_tuple(fmt, args...)](auto &&output_it) -> void {
+    std::apply([](auto &&o, fmt::string_view fmt, auto &&...args) { fmt::vformat_to(o, fmt, fmt::make_format_args(args...)); }, std::tuple_cat(std::tie(output_it), t));
+  };
+}
+
+}  // namespace coring::detail
+
+namespace coring {
 // use in anonymous object
 class logger {
   // compile time calculation of basename of source file
  public:
-  typedef void (*async_interface_t)(const char *line, int len);
   enum log_level {
-    TRACE,
-    DEBUG,
-    INFO,
-    WARN,
-    ERROR,
-    FATAL,
+    TRACE = 0,
+    DEBUG = 1,
+    INFO = 2,
+    WARN = 3,
+    ERROR = 4,
+    FATAL = 5,
     LEVELS_CNT,
   };
-  using file_name_t = detail::file_name_converter;
+
+  /// This class is used to implicit convert a file name
+  /// to it's basename in compile time.
+  class file_name_converter {
+   public:
+    template <int N>
+    file_name_converter(const char (&arr)[N]) : data_(arr), size_(N - 1) {  // NOLINT(google-explicit-constructor)
+      const char *slash = strrchr(data_, '/');                              // builtin function
+      if (slash) {
+        data_ = slash + 1;
+        size_ -= static_cast<int>(data_ - arr);
+      }
+    }
+
+    explicit file_name_converter(const char *filename) : data_(filename) {
+      const char *slash = strrchr(filename, '/');
+      if (slash) {
+        data_ = slash + 1;
+      }
+      size_ = static_cast<int>(strlen(data_));
+    }
+
+    const char *data_;
+    int size_;
+  };
+
+  using file_name_t = file_name_converter;
+
   // use in anonymous object
-  logger(file_name_t file, int line, log_level level = INFO) {}
-  ~logger() {}
+  logger(file_name_t file, int line, log_level level = INFO);
+
+  // trivial
+  ~logger() = default;
+
+  template <typename... Args>
+  auto try_log(fmt::format_string<Args...> &&fmt, Args &&...args) {
+    return detail::make_log_task(std::move(fmt), args...);
+  }
 
  private:
-  detail::logger_impl impl;
-  static async_interface_t output;
+  friend TEST;
+  class log_entry {
+   public:
+    // compiler optimizes it
+    log_entry(file_name_t file, int line, timestamp ts, log_level lv);
+
+   private:
+    friend TEST;
+    file_name_t file_;
+    int line_;
+    timestamp ts_;
+    log_level lv_;
+  };
+  log_entry log_entry_;
+  typedef std::back_insert_iterator<std::vector<char>> output_iterator_t;
+  typedef void (*submit_interface)(std::function<void(output_iterator_t)>, log_entry);
+
+  const static char *log_level_map_[logger::LEVELS_CNT];
+  static submit_interface submit_async;
 };
 
 }  // namespace coring
