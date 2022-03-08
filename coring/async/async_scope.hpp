e3a78b4615a401c252b8e66c5e33ddff8a043920
pan <panjunzhong@outlook.com>
2022-03-08 23:12:57 +0800

    chore(cppcoro): g++ reports that final_suspend need to be noexcept

--- coring/async/async_scope.hpp
+++ coring/async/async_scope.hpp
@@ -66,7 +66,7 @@ class async_scope {
   struct oneway_task {
     struct promise_type {
       std::suspend_never initial_suspend() { return {}; }
-      std::suspend_never final_suspend() { return {}; }
+      std::suspend_never final_suspend() noexcept { return {}; }
       void unhandled_exception() { std::terminate(); }
       oneway_task get_return_object() { return {}; }
       void return_void() {}
