612133eaad83c575003b11899304c48f2f801941
pan <panjunzhong@outlook.com>
2022-03-14 14:53:53 +0800

    func(executor): I forget to upload this file, it's just a function wrapper of executor methods

---
+++ coring/utils/execute.hpp
@@ -0,0 +1,20 @@
+
+#ifndef CORING_EXECUTE_HPP
+#define CORING_EXECUTE_HPP
+
+#include "thread.hpp"
+namespace coring {
+template <typename Exec, typename Task>
+void execute(Exec *exec, Task &&task) {
+  exec->execute(std::forward<Task>(task));
+}
+template <typename Exec, typename Task>
+void schedule(Exec *exec, Task &&task) {
+  exec->schedule(std::forward<Task>(task));
+}
+template <typename Exec>
+void submit(Exec *exec) {
+  exec->submit();
+}
+}  // namespace coring
+#endif  // CORING_EXECUTE_HPP
